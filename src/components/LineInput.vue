fc764f36fb9727d77e8086c16a625a7d0b9d56d0
Nathan Strutz <strutz@gmail.com>
2019-10-28 00:40:34 -0400

    Applying the first piece of data... not sure I like it, probably going to change this strategy up a bit.

---
+++ src/components/LineInput.vue
@@ -0,0 +1,24 @@
+<template>
+	<tr class="line-input">
+		<td>
+			<label>{{ title }}:</label>
+			<input type="text" :value="text" @input="$emit('updateText', $event.target.value)" />
+		</td>
+		<td>
+			<input type="text" class="numeric" :value="value" @input="$emit('updateValue', $event.target.value)" />
+		</td>
+	</tr>
+</template>
+
+<script>
+export default {
+	props: {
+		title: String,
+		text: String,
+		value: Number
+	}
+};
+</script>
+
+<style>
+</style>
--- src/sections/CashflowStatementIncome.vue
+++ src/sections/CashflowStatementIncome.vue
@@ -1,106 +1,119 @@
 <template>
-  <div class="income">
-    <h3 class="column-title-bar">Income</h3>
-    <table class="line-inputs">
-      <thead>
-        <tr class="column-header">
-          <th>Description</th>
-          <th>Cash Flow</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr class="line-input">
-          <td>
-            <label>Salary:</label>
-            <input type="text" id="salary" />
-          </td>
-          <td>
-            <input type="text" class="income" />
-          </td>
-        </tr>
-        <tr class="line-input">
-          <td>
-            <label>Interest:</label>
-            <input type="text" id="interest" />
-          </td>
-          <td>
-            <input type="text" class="income" />
-          </td>
-        </tr>
-        <tr class="line-input">
-          <td>
-            <label>Dividends:</label>
-            <input type="text" id="dividends" />
-          </td>
-          <td>
-            <input type="text" class="income" />
-          </td>
-        </tr>
-        <tr class="blank-line-input">
-          <td>
-            <input type="text" id="salary" />
-          </td>
-          <td>
-            <input type="text" class="income" />
-          </td>
-        </tr>
-        <tr class="line-input real-estate">
-          <td>
-            <label>Real Estate:</label>
-            <input type="text" />
-          </td>
-          <td>
-            <input type="text" class="income" />
-          </td>
-        </tr>
-        <tr class="blank-line-input real-estate">
-          <td>
-            <input type="text" />
-          </td>
-          <td>
-            <input type="text" class="income" />
-          </td>
-        </tr>
-        <tr class="blank-line-input real-estate">
-          <td>
-            <input type="text" />
-          </td>
-          <td>
-            <input type="text" class="income" />
-          </td>
-        </tr>
-        <tr class="blank-line-input real-estate">
-          <td>
-            <input type="text" />
-          </td>
-          <td>
-            <input type="text" class="income" />
-          </td>
-        </tr>
-        <tr class="line-input real-estate">
-          <td>
-            <label>Businesses:</label>
-            <input type="text" />
-          </td>
-          <td>
-            <input type="text" class="income" />
-          </td>
-        </tr>
-        <tr class="blank-line-input real-estate">
-          <td>
-            <input type="text" />
-          </td>
-          <td>
-            <input type="text" class="income" />
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+	<div class="income">
+		<h3 class="column-title-bar">Income</h3>
+		<table class="line-inputs">
+			<thead>
+				<tr class="column-header">
+					<th>Description</th>
+					<th>Cash Flow</th>
+				</tr>
+			</thead>
+			<tbody>
+				<line-input
+					title="Salary"
+					:text="income.salary.note"
+					@updateText="val => changeSalary({ note: val })"
+					:value="income.salary.value"
+					@updateValue="val => changeSalary({ value: val })"
+				/>
+				<tr class="line-input">
+					<td>
+						<label>Interest:</label>
+						<input type="text" id="interest" />
+					</td>
+					<td>
+						<input type="text" class="income" />
+					</td>
+				</tr>
+				<tr class="line-input">
+					<td>
+						<label>Dividends:</label>
+						<input type="text" id="dividends" />
+					</td>
+					<td>
+						<input type="text" class="income" />
+					</td>
+				</tr>
+				<tr class="blank-line-input">
+					<td>
+						<input type="text" id="salary" />
+					</td>
+					<td>
+						<input type="text" class="income" />
+					</td>
+				</tr>
+				<tr class="line-input real-estate">
+					<td>
+						<label>Real Estate:</label>
+						<input type="text" />
+					</td>
+					<td>
+						<input type="text" class="income" />
+					</td>
+				</tr>
+				<tr class="blank-line-input real-estate">
+					<td>
+						<input type="text" />
+					</td>
+					<td>
+						<input type="text" class="income" />
+					</td>
+				</tr>
+				<tr class="blank-line-input real-estate">
+					<td>
+						<input type="text" />
+					</td>
+					<td>
+						<input type="text" class="income" />
+					</td>
+				</tr>
+				<tr class="blank-line-input real-estate">
+					<td>
+						<input type="text" />
+					</td>
+					<td>
+						<input type="text" class="income" />
+					</td>
+				</tr>
+				<tr class="line-input real-estate">
+					<td>
+						<label>Businesses:</label>
+						<input type="text" />
+					</td>
+					<td>
+						<input type="text" class="income" />
+					</td>
+				</tr>
+				<tr class="blank-line-input real-estate">
+					<td>
+						<input type="text" />
+					</td>
+					<td>
+						<input type="text" class="income" />
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 </template>
 
 <script>
-export default {};
+import LineInput from "@/components/LineInput.vue";
+import BlankLineInput from "@/components/BlankLineInput.vue";
+import { mapState, mapMutations } from "vuex";
+
+export default {
+	components: {
+		LineInput,
+		BlankLineInput
+	},
+	computed: {
+		...mapState(["income"])
+	},
+	methods: {
+		...mapMutations(["changeSalary"])
+	}
+};
 </script>
 
 <style lang="scss">
