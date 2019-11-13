84c456f57be74743f27d119353d4ec73c3ac24de
Nathan Strutz <strutz@gmail.com>
2019-11-12 23:37:39 -0500

    Joined stock/mutual/cd values into the store

--- src/sections/BalanceSheetAssets.vue
+++ src/sections/BalanceSheetAssets.vue
@@ -1,106 +1,100 @@
 <template>
-	<div id="assets">
-		<h3 class="column-title-bar">Assets</h3>
-		<div id="asset-list">
-			<div class="line-input">
-				<label for="savings">Savings:</label>
-				<input type="text" id="savings" class="numeric" :value="savings" @input="changeSavings($event.target.value)" />
-			</div>
-			<div class="line-input all-text">
-				<div>Stocks/Mutual's/CDs</div>
-				<div>No. of Shares:</div>
-				<div>Cost/Share:</div>
-			</div>
-			<div class="line-input">
-				<input type="text" />
-				<input type="text" class="numeric" />
-				<input type="text" class="numeric" />
-			</div>
-			<div class="line-input">
-				<input type="text" />
-				<input type="text" class="numeric" />
-				<input type="text" class="numeric" />
-			</div>
-			<div class="line-input all-text">
-				<div>Real Estate:</div>
-				<div>Down Pay:</div>
-				<div>Cost:</div>
-			</div>
-			<div class="line-input">
-				<input type="text" />
-				<input type="text" class="numeric" />
-				<input type="text" class="numeric" />
-			</div>
-			<div class="line-input">
-				<input type="text" />
-				<input type="text" class="numeric" />
-				<input type="text" class="numeric" />
-			</div>
-			<div class="line-input">
-				<input type="text" />
-				<input type="text" class="numeric" />
-				<input type="text" class="numeric" />
-			</div>
-			<div class="line-input">
-				<input type="text" />
-				<input type="text" class="numeric" />
-				<input type="text" class="numeric" />
-			</div>
-			<div class="line-input all-text">
-				<div>Business:</div>
-				<div>Down Pay:</div>
-				<div>Cost:</div>
-			</div>
-			<div class="line-input">
-				<input type="text" />
-				<input type="text" class="numeric" />
-				<input type="text" class="numeric" />
-			</div>
-			<div class="line-input">
-				<input type="text" />
-				<input type="text" class="numeric" />
-				<input type="text" class="numeric" />
-			</div>
-		</div>
-	</div>
+  <div id="assets">
+    <h3 class="column-title-bar">Assets</h3>
+    <div id="asset-list">
+      <div class="line-input">
+        <label for="savings">Savings:</label>
+        <input type="text" id="savings" class="numeric" :value="savings" @input="changeSavings($event.target.value)" />
+      </div>
+      <div class="line-input all-text">
+        <div>Stocks/Mutual's/CDs</div>
+        <div>No. of Shares:</div>
+        <div>Cost/Share:</div>
+      </div>
+      <stock-mutual-cd-asset-row v-for="(stock, index) in stocks" :key="index" :index="index" />
+      <div class="line-input all-text">
+        <div>Real Estate:</div>
+        <div>Down Pay:</div>
+        <div>Cost:</div>
+      </div>
+      <div class="line-input">
+        <input type="text" />
+        <input type="text" class="numeric" />
+        <input type="text" class="numeric" />
+      </div>
+      <div class="line-input">
+        <input type="text" />
+        <input type="text" class="numeric" />
+        <input type="text" class="numeric" />
+      </div>
+      <div class="line-input">
+        <input type="text" />
+        <input type="text" class="numeric" />
+        <input type="text" class="numeric" />
+      </div>
+      <div class="line-input">
+        <input type="text" />
+        <input type="text" class="numeric" />
+        <input type="text" class="numeric" />
+      </div>
+      <div class="line-input all-text">
+        <div>Business:</div>
+        <div>Down Pay:</div>
+        <div>Cost:</div>
+      </div>
+      <div class="line-input">
+        <input type="text" />
+        <input type="text" class="numeric" />
+        <input type="text" class="numeric" />
+      </div>
+      <div class="line-input">
+        <input type="text" />
+        <input type="text" class="numeric" />
+        <input type="text" class="numeric" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import { mapState, mapMutations } from "vuex";
+import StockMutualCdAssetRow from "@/components/StockMutualCdAssetRow.vue";
 
 export default {
-	computed: {
-		...mapState("assets", ["savings"])
-	},
-	methods: {
-		...mapMutations("assets", ["changeSavings"])
-	}
+  components: { StockMutualCdAssetRow },
+  computed: {
+    ...mapState("assets", ["savings"]),
+    ...mapState("investments", ["stocks"])
+  },
+  methods: {
+    ...mapMutations("assets", ["changeSavings"])
+  }
 };
 </script>
 
 <style lang="scss">
 #assets {
-	grid-area: assets;
+  grid-area: assets;
 }
 
 #asset-list {
-	display: flex;
-	flex-wrap: nowrap;
-	flex-direction: column;
-	justify-content: flex-start;
+  display: flex;
+  flex-wrap: nowrap;
+  flex-direction: column;
+  justify-content: flex-start;
 }
 
 #asset-list > * {
-	/* In relation to its parent */
-	flex: 1 1 auto;
-	margin: 7px;
-	/* in relation to its children */
-	display: flex;
-	flex-wrap: nowrap;
-	flex-direction: row;
+  /* In relation to its parent */
+  flex: 1 1 auto;
+  margin: 7px;
+  /* in relation to its children */
+  display: flex;
+  flex-wrap: nowrap;
+  flex-direction: row;
 }
 #asset-list > * > * {
-	flex: 1 1 auto;
-	white-space: nowrap;
+  flex: 1 1 auto;
+  white-space: nowrap;
 }
 </style>
