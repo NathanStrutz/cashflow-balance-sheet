1ce18b01b6a44c84059f3b4540ae59334a576f81
Nathan Strutz <strutz@gmail.com>
2019-11-13 00:48:58 -0500

    Entire income area is in state

--- src/sections/CashflowStatementIncome.vue
+++ src/sections/CashflowStatementIncome.vue
@@ -1,86 +1,22 @@
 <template>
-	<div class="income">
-		<h3 class="column-title-bar">Income</h3>
-		<table class="line-inputs">
-			<thead>
-				<tr class="column-header">
-					<th>Description</th>
-					<th>Cash Flow</th>
-				</tr>
-			</thead>
-			<tbody>
-				<state-connected-input category="income" title="Salary" item="salary" change="changeSalary" />
-				<state-connected-input category="income" title="Interest" item="interest" change="changeInterest" />
-				<tr class="line-input">
-					<td>
-						<label>Dividends:</label>
-						<input type="text" id="dividends" />
-					</td>
-					<td>
-						<input type="text" class="income" />
-					</td>
-				</tr>
-				<tr class="blank-line-input">
-					<td>
-						<input type="text" id="salary" />
-					</td>
-					<td>
-						<input type="text" class="income" />
-					</td>
-				</tr>
-				<tr class="line-input real-estate">
-					<td>
-						<label>Real Estate:</label>
-						<input type="text" />
-					</td>
-					<td>
-						<input type="text" class="income" />
-					</td>
-				</tr>
-				<tr class="blank-line-input real-estate">
-					<td>
-						<input type="text" />
-					</td>
-					<td>
-						<input type="text" class="income" />
-					</td>
-				</tr>
-				<tr class="blank-line-input real-estate">
-					<td>
-						<input type="text" />
-					</td>
-					<td>
-						<input type="text" class="income" />
-					</td>
-				</tr>
-				<tr class="blank-line-input real-estate">
-					<td>
-						<input type="text" />
-					</td>
-					<td>
-						<input type="text" class="income" />
-					</td>
-				</tr>
-				<tr class="line-input real-estate">
-					<td>
-						<label>Businesses:</label>
-						<input type="text" />
-					</td>
-					<td>
-						<input type="text" class="income" />
-					</td>
-				</tr>
-				<tr class="blank-line-input real-estate">
-					<td>
-						<input type="text" />
-					</td>
-					<td>
-						<input type="text" class="income" />
-					</td>
-				</tr>
-			</tbody>
-		</table>
-	</div>
+  <div class="income">
+    <h3 class="column-title-bar">Income</h3>
+    <table class="line-inputs">
+      <thead>
+        <tr class="column-header">
+          <th>Description</th>
+          <th>Cash Flow</th>
+        </tr>
+      </thead>
+      <tbody>
+        <state-connected-input category="income" title="Salary" item="salary" change="changeSalary" />
+        <state-connected-input category="income" title="Interest" item="interest" change="changeInterest" />
+        <state-connected-input category="income" item="interest2" change="changeInterest2" />
+      </tbody>
+      <income-real-estate />
+      <income-businesses />
+    </table>
+  </div>
 </template>
 
 <script>
@@ -88,17 +24,22 @@ import StateConnectedInput from "@/components/StateConnectedInput.vue";
 import BlankLineInput from "@/components/BlankLineInput.vue";
 import { mapState, mapMutations } from "vuex";
 
+import IncomeRealEstate from "./income/IncomeRealEstate.vue";
+import IncomeBusinesses from "./income/IncomeBusinesses.vue";
+
 export default {
-	components: {
-		StateConnectedInput
-	},
-	computed: {},
-	methods: {}
+  components: {
+    StateConnectedInput,
+    IncomeRealEstate,
+    IncomeBusinesses
+  },
+  computed: {},
+  methods: {}
 };
 </script>
 
 <style lang="scss">
 #income {
-	grid-area: income;
+  grid-area: income;
 }
 </style>
