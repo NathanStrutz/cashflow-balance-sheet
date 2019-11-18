4509f9aed8c893d50982d918874a1fcf29c57bf6
Nathan Strutz <strutz@gmail.com>
2019-11-17 21:33:42 -0500

    Fix for zeros that should have been on liability inputs

--- src/components/BalanceSheetLiabilities.vue
+++ src/components/BalanceSheetLiabilities.vue
@@ -3,28 +3,28 @@
 		<h3 class="column-title-bar">Liabilities</h3>
 		<table id="liability-list" class="line-inputs">
 			<tbody>
-				<state-connected-input title="Home Mortgage" category="liabilities" item="mortgage" change="changeMortgage" />
-				<state-connected-input title="School Loans" category="liabilities" item="schoolLoan" change="changeSchoolLoan" />
-				<state-connected-input title="Car Loans" category="liabilities" item="carLoan" change="changeCarLoan" />
-				<state-connected-input title="Credit Cards" category="liabilities" item="creditCard" change="changeCreditCard" />
-				<state-connected-input title="Retail Debt" category="liabilities" item="retail" change="changeRetail" />
+				<state-connected-value-input title="Home Mortgage" category="liabilities" item="mortgage" change="changeMortgage" />
+				<state-connected-value-input title="School Loans" category="liabilities" item="schoolLoan" change="changeSchoolLoan" />
+				<state-connected-value-input title="Car Loans" category="liabilities" item="carLoan" change="changeCarLoan" />
+				<state-connected-value-input title="Credit Cards" category="liabilities" item="creditCard" change="changeCreditCard" />
+				<state-connected-value-input title="Retail Debt" category="liabilities" item="retail" change="changeRetail" />
 			</tbody>
 			<liability-real-estate />
 			<liability-businesses />
 			<tbody>
-				<state-connected-input title="Bank Loan" category="liabilities" item="bankLoan" change="changeBankLoan" />
+				<state-connected-value-input title="Bank Loan" category="liabilities" item="bankLoan" change="changeBankLoan" />
 			</tbody>
 		</table>
 	</div>
 </template>
 
 <script>
-import StateConnectedInput from "./misc/StateConnectedInput.vue";
+import StateConnectedValueInput from "./misc/StateConnectedValueInput.vue";
 import LiabilityRealEstate from "./liability/LiabilityRealEstate.vue";
 import LiabilityBusinesses from "./liability/LiabilityBusinesses.vue";
 
 export default {
-	components: { StateConnectedInput, LiabilityRealEstate, LiabilityBusinesses }
+	components: { StateConnectedValueInput, LiabilityRealEstate, LiabilityBusinesses }
 };
 </script>
 
---
+++ src/components/misc/StateConnectedValueInput.vue
@@ -0,0 +1,31 @@
+<template>
+	<line-input :title="title" :value="value" @updateValue="changeValue" :hasText="false" />
+</template>
+
+<script>
+import { mapState, mapMutations } from "vuex";
+import LineInput from "./LineInput.vue";
+
+export default {
+	components: {
+		LineInput
+	},
+	props: {
+		title: String,
+		category: String,
+		item: String,
+		change: String // State change method for the item
+	},
+	computed: {
+		/** the node for this category/item */
+		value() {
+			return this.$store.state[this.category][this.item];
+		}
+	},
+	methods: {
+		changeValue(e) {
+			this.$store.commit(this.category + "/" + this.change, { value: Number(e) });
+		}
+	}
+};
+</script>
