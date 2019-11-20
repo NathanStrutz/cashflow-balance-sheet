e5fddfd3687ffe9508e99ef8ed6219ad9a4a5fb1
Nathan Strutz <strutz@gmail.com>
2019-11-20 00:13:39 -0500

    Fix missing child expenses

--- src/components/CashflowStatementExpenses.vue
+++ src/components/CashflowStatementExpenses.vue
@@ -30,7 +30,7 @@ export default {
 	},
 	computed: {
 		...mapState("expenses", ["children"]),
-		...mapState(["childExpenses"])
+		...mapGetters(["childExpenses"])
 	},
 	methods: {
 		...mapMutations("expenses", ["changeChildNote"])
